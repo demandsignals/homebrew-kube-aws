2a9d301392f02721e6569c158fc9d0d1b2933154
Roman Pearah <roman.pearah@demandsignals.com>
2016-04-12 10:50:44 -0400

    Update kube-aws version to v0.6.0

--- kube-aws.rb
+++ kube-aws.rb
@@ -1,9 +1,9 @@
 class KubeAws < Formula
   desc "Kubernetes on AWS"
   homepage "https://github.com/coreos/coreos-kubernetes/tree/master/multi-node/aws"
-  url "https://github.com/coreos/coreos-kubernetes/releases/download/v0.2.0/kube-aws-darwin-amd64.tar.gz"
-  version "v0.2.0"
-  sha256 "0286313278fc5a1a7e40945bd0e657ab3fb98aba1d9c414939ebccf2854d3def"
+  url "https://github.com/coreos/coreos-kubernetes/releases/download/v0.6.0/kube-aws-darwin-amd64.tar.gz"
+  version "v0.6.0"
+  sha256 "b2e64fcc0da559a13ebae91eb6ea76796f5111c5fbf50766a6a9af0f4a6d64e4"
 
   def install
     bin.install "kube-aws"
